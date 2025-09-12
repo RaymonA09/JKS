da49f85049364ec6ed989e0e9b8d504a3f914238
RaymonA09 <ralphaurelio1@gmail.com>
2025-09-13 01:23:56 +0800

    Update project for Deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import tailwindcss from '@tailwindcss/vite'
-
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -11,4 +10,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
+  base: '/JKS/', // 👈 this is the key fix for GitHub Pages
 });
